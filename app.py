95a3ab9219635d1cd647cf2b5cd0307d063dbe9e
bsdcpp <bsdcpp@gmail.com>
2022-11-19 21:30:16 +0800

    add fruits

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import random
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    fruits_list = ['苹果', '香蕉', '榴莲', '石榴', '柳橙', '芒果', '百香果', '葡萄']
+    fruits = random.sample(fruits_list, random.randrange(5, 7))
+    return render_template('index.html', fruits=fruits)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
